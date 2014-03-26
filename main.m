46fe04e9fe2a00956f8dad0e35fb0640be585637
Brad Smith <Brad_Smith@intuit.com>
2014-03-26 12:57:21 -0700

    Deletes everything

--- main.m
+++ main.m
@@ -5,14 +5,3 @@
 //  Copyright (c) 2013 Tyler Fox
 //  https://github.com/smileyborg/UIView-AutoLayout
 //
-
-#import <UIKit/UIKit.h>
-
-#import test
-
-int main(int argc, char *argv[])
-{
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ALAppDelegate class]));
-    }
-}
